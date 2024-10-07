19967b5d50fbe5f09a69d487f52ad750c7a5c93b
e2302260 <e2302260@edu.vamk.fi>
2024-10-07 09:32:54 +0300

    Tehtävä 7

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import Expenses from './components/Expenses';
+import React from 'react';
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
 
@@ -13,19 +14,19 @@ const expenses = [
   id: 'e2',
   title: 'New TV',
   amount: 799.49,
-  date: new Date("2023-01-12"),
+  date: new Date("2023-08-12"),
  },
  { 
   id: 'e3',
   title: 'Car Insurance',
   amount: 300.00,
-  date: new Date("2023-01-11"),
+  date: new Date("2023-01-01"),
  },
 { 
   id: 'e4',
   title: 'New Desk (Wooden)',
   amount: 450,
-  date: new Date("2022-05-12"),
+  date: new Date("2022-01-09"),
  },
 ];
 
