2832bdc89a7d215cfa0b502797145408815cc4b5
e2302260 <e2302260@edu.vamk.fi>
2024-10-28 08:56:37 +0200

    Tehtävä 18

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState} from 'react';
 
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense =  (props) => {
+
+    const [isEditing, setIsEditing] = useState(false);
 
     const SaveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
@@ -11,11 +13,28 @@ const NewExpense =  (props) => {
             id: Math.random().toString()
         };
         props.onAddExpense(expenseData);
+        setIsEditing(false);
+    };
+
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    };
+    
+    const stopEditingHandler = () => {
+        setIsEditing(false);
     };
     
     return (
         <div className ="new-expense">
-            <ExpenseForm onSaveExpenseData={SaveExpenseDataHandler} />
+            {!isEditing && (
+                <button onClick={startEditingHandler}>Add New Expense</button>
+            )}
+            {isEditing && (
+                <ExpenseForm 
+                onSaveExpenseData={SaveExpenseDataHandler}
+                onCancel={stopEditingHandler}
+                />
+            )}
         </div>
     );
 };
