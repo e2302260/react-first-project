7421b8c3faa2ccb46ec1c0edef4044e9353e3ce8
e2302260 <e2302260@edu.vamk.fi>
2024-10-28 07:55:14 +0200

    Tehtävä 16

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,24 +14,30 @@ const Expenses = (props) => {
       setFilteredYear(selectedYear);
   };
 
-  const filteredExpenses = props.items.filter(expense => {
-    return expense.date.getFullYear().toString() === filteredYear;
-  });
+const filteredExpenses = props.items.filter(expense => {
+  return expense.date.getFullYear().toString() === filteredYear;
+});
+
+let expensesContent = <p>No expenses found.</p>;
+
+if (filteredExpenses.length > 0) {
+  expensesContent = filteredExpenses.map((expense) => (
+    <ExpenseItem
+    key={expense.id}
+    title={expense.title}
+    amount={expense.amount}
+    date={expense.date}
+    />
+  ));
+}
 
  return (
     <Card className ="expenses">
-      <ExpensesFilter 
-      selected={filteredYear} 
-      onChangeFilter={filterChangeHandler}/>
-      {filteredExpenses.map((expense) => (
-       //props.items.map((expense) => (
-        <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-        />
-      ))}
+     <ExpensesFilter
+      selected={filteredYear}
+      onChangeFilter={filterChangeHandler}
+     />
+     {expensesContent}
     </Card>
   );
 }
